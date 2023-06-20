958e5ccec1aff5048d1f7f71877db12fd8b48322
Mina Tehseen <mina.thsn09@gmail.com>
2023-06-20 13:25:25 +0500

    Initial Commit

---
+++ queries.sql
@@ -0,0 +1,16 @@
+CREATE TABLE Users (
+    PersonID int NOT NULL AUTO_INCREMENT,
+    UserName varchar(255),
+    Age int,
+    City varchar(255),
+    Country varchar(255),
+    Profession varchar(255),
+);
+
+
+ INSERT INTO Users (UserName, Age, City, Country, Profession)
+ VALUES 
+ ('Harry',31,'New York','US','Doctor'),
+ ('Taylor',26,'Toronto','Canada','Engineer'),
+ ('Karen',43,'Sydney','Australia','Nurse'),
+ ('Zendeya',55,'Cairo','Egypt','Chef');
