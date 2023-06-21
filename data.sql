a0cc437b0029cd8e8536ccc67fa6da5aac9e4ac7
Mina Tehseen <mina.thsn09@gmail.com>
2023-06-21 13:14:05 +0500

    Refactored Code

---
+++ data.sql
@@ -0,0 +1,9 @@
+
+
+INSERT INTO students (name, age, city, country, profession)
+ VALUES 
+ ('Harry',31,'New York','US','Doctor'),
+ ('Taylor',26,'Toronto','Canada','Engineer'),
+ ('Karen',43,'Sydney','Australia','Nurse'),
+ ('Zendeya',55,'Cairo','Egypt','Chef');
+
--- schema.sql
+++ schema.sql
@@ -1,14 +1,8 @@
-CREATE TABLE IF NOT EXISTS Users (
-    UserName varchar(255),
-    Age int,
-    City varchar(255),
-    Country varchar(255),
-    Profession varchar(255)
+CREATE TABLE IF NOT EXISTS students (
+    name varchar(255),
+    age int,
+    city varchar(255),
+    country varchar(255),
+    profession varchar(255)
 );
 
-INSERT INTO Users (UserName, Age, City, Country, Profession)
- VALUES 
- ('Harry',31,'New York','US','Doctor'),
- ('Taylor',26,'Toronto','Canada','Engineer'),
- ('Karen',43,'Sydney','Australia','Nurse'),
- ('Zendeya',55,'Cairo','Egypt','Chef');
